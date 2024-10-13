f0b239afd5456451b4b1c3d99ff2840e8308ce7c
Nguyễn Mạnh Tuấn <tuanmnguye@northstudio.vn>
2024-10-13 14:17:16 +0700

    :pencil: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Project Management')
+    .setDescription('Project Management API description')
+    .setVersion('1.0')
+    .addTag('PM')
+    .addBearerAuth(
+      {
+        description: `[just text field] Please enter token in following format: Bearer <JWT>`,
+        name: "Authorization",
+        bearerFormat: "Bearer",
+        scheme: "Bearer",
+        type: "http",
+        in: "Header"
+      }
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, {
+    swaggerOptions: {
+      persistAuthorization: true,
+      uiConfig: {
+        docExpansion: 'none'
+      },
+      docExpansion: 'none',
+    },
+  });
+
   await app.listen(3000);
-  
 }
 bootstrap();
