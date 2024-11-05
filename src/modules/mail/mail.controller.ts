fa6aa4be6d29a682a15e14059d342077cab45576
tuaans <manhtuan17403@gmail.com>
2024-11-05 17:47:07 +0700

    :sparkles: test mail

--- src/modules/mail/mail.controller.ts
+++ src/modules/mail/mail.controller.ts
@@ -1,12 +1,17 @@
+import { DbService } from "@/base/db/services";
 import { Controller, Post, Request } from "@nestjs/common";
 
 @Controller("mails")
 export class MailController {
+  constructor(
+    private db: DbService
+  ) {}
+
   @Post('/postmark-webhook')
   async receiveEmail (
     @Request() req
   ) {
-
+    await this.db.email.create(req.body)
     console.log(req.body)
     return {
       message: ""
