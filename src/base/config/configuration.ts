2e565f77815ee4eadcb8bf934f52c6a1bbe7dedb
tuanmn <manhtuan17403@gmail.com>
2024-12-22 16:35:08 +0700

    :recycle: migrate aws sdk v2 to v3

--- src/base/config/configuration.ts
+++ src/base/config/configuration.ts
@@ -8,7 +8,8 @@ export const configuration = () => ({
     s3: {
       accessKeyId: process.env.S3_ACCESS_KEY_ID,
       secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-      bucketName: process.env.S3_BUCKET_NAME
+      bucketName: process.env.S3_BUCKET_NAME,
+      region: process.env.S3_REGION
     },
     cloudinary: {
       apikey: process.env.CLOUDINARY_API_KEY,
--- src/base/services/storage.service.ts
+++ src/base/services/storage.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from "@nestjs/common";
-import * as AWS from "aws-sdk";
 import { ConfigService } from "@nestjs/config";
 import sharp from "sharp";
 import axios from "axios";
@@ -7,148 +6,146 @@ import { HydratedDocument } from "mongoose";
 import { DbService } from "../db/services/db.service";
 import { randomString } from "@/common/utils";
 import { ProjectAttachment, ProjectAttachmentWithDownloadUrl, Task } from "../db";
+import { S3Client, PutObjectCommand, GetObjectCommand } from '@aws-sdk/client-s3'
 
 @Injectable()
 export class StorageService {
-  s3: AWS.S3;
+  s3: S3Client;
   s3Bucket: string;
+  s3Region: string;
 
   constructor(
     private config: ConfigService,
     private db: DbService
   ) {
-    this.s3 = new AWS.S3({
+    this.s3 = new S3Client({
+      region: this.config.get("storage.s3.region"),
       credentials: {
         accessKeyId: this.config.get("storage.s3.accessKeyId"),
         secretAccessKey: this.config.get("storage.s3.secretAccessKey")
       }
     });
     this.s3Bucket = this.config.get("storage.s3.bucketName");
+    this.s3Region = this.config.get("storage.s3.region")
   }
 
-  async deleteFile(fileKey: string) {
-    return this.s3.deleteObject({
-      Bucket: this.s3Bucket,
-      Key: fileKey
-    }).promise();
-  }
+  // async deleteFile(fileKey: string) {
+  //   return this.s3.deleteObject({
+  //     Bucket: this.s3Bucket,
+  //     Key: fileKey
+  //   }).promise();
+  // }
 
-  async uploadAttachmentFile(spaceId: string, memberId: string, file: Express.Multer.File) {
+  async uploadAttachmentFile(projectId: string, file: Express.Multer.File) {
     const timestamp = new Date().getTime();
-    return await this.s3.upload({
-      Bucket: this.s3Bucket,
-      Key: `spaces/${spaceId}/${memberId}/${timestamp}_${file.originalname}`,
-      Body: file.buffer,
-      ACL: "private",
-      ContentType: file.mimetype,
-      ContentDisposition: "inline"
-    }).promise();
+    const key = `projects/${projectId}/${timestamp}_${file.originalname}`
+    await this.s3.send(
+      new PutObjectCommand({
+        Bucket: this.s3Bucket,
+        Key: key,
+        Body: file.buffer,
+        ACL: "public-read",
+        ContentType: file.mimetype
+      }))
+    return { url: `https://${this.s3Bucket}.s3.${this.s3Region}.amazonaws.com/${key}` }
   }
 
-  async uploadEmailAttachment(contentType: string, fileName: string, cid: string, size: number, attachmentUrl: string) {
-    const response = await axios.get(attachmentUrl, {
-      responseType: "arraybuffer",
-      auth: {
-        username: "api",
-        password: this.config.get("mail.mailgunKey")
-      }
-    });
-    const buffer = Buffer.from(response.data, "binary");
-    const storageKey = `emails/attachments/${cid}_${randomString(10)}_${fileName}`;
-    const newUpload = await this.s3.upload({
-      Bucket: this.s3Bucket,
-      Key: storageKey,
-      Body: buffer,
-      ACL: "private",
-      ContentType: contentType
-    }).promise();
+  // async uploadEmailAttachment(contentType: string, fileName: string, cid: string, size: number, attachmentUrl: string) {
+  //   const response = await axios.get(attachmentUrl, {
+  //     responseType: "arraybuffer",
+  //     auth: {
+  //       username: "api",
+  //       password: this.config.get("mail.mailgunKey")
+  //     }
+  //   });
+  //   const buffer = Buffer.from(response.data, "binary");
+  //   const storageKey = `emails/attachments/${cid}_${randomString(10)}_${fileName}`;
+  //   const newUpload = await this.s3.upload({
+  //     Bucket: this.s3Bucket,
+  //     Key: storageKey,
+  //     Body: buffer,
+  //     ACL: "private",
+  //     ContentType: contentType
+  //   }).promise();
 
-    return this.db.emailAttachment.create({
-      contentType,
-      name: fileName,
-      url: newUpload.Location,
-      size,
-      cid,
-      storageKey
-    });
-  }
+  //   return this.db.emailAttachment.create({
+  //     contentType,
+  //     name: fileName,
+  //     url: newUpload.Location,
+  //     size,
+  //     cid,
+  //     storageKey
+  //   });
+  // }
 
-  async uploadEmailAttachmentBuffer(contentType: string, fileName: string, cid: string, size: number, attachment: Buffer) {
-    const storageKey = `emails/attachments/${cid}_${randomString(10)}_${fileName}`;
-    const newUpload = await this.s3.upload({
-      Bucket: this.s3Bucket,
-      Key: storageKey,
-      Body: attachment,
-      ACL: "private",
-      ContentType: contentType
-    }).promise();
+  // async uploadEmailAttachmentBuffer(contentType: string, fileName: string, cid: string, size: number, attachment: Buffer) {
+  //   const storageKey = `emails/attachments/${cid}_${randomString(10)}_${fileName}`;
+  //   const newUpload = await this.s3.upload({
+  //     Bucket: this.s3Bucket,
+  //     Key: storageKey,
+  //     Body: attachment,
+  //     ACL: "private",
+  //     ContentType: contentType
+  //   }).promise();
 
-    return this.db.emailAttachment.create({
-      contentType,
-      name: fileName,
-      url: newUpload.Location,
-      size,
-      cid,
-      storageKey
-    });
-  }
+  //   return this.db.emailAttachment.create({
+  //     contentType,
+  //     name: fileName,
+  //     url: newUpload.Location,
+  //     size,
+  //     cid,
+  //     storageKey
+  //   });
+  // }
 
-  async getSignedUrl(key: string, expiration = 3600) {
-    return this.s3.getSignedUrl("getObject", {
-      Bucket: this.s3Bucket,
-      Key: key,
-      Expires: expiration
-    });
-  }
+  // async uploadProjectCoverFile(file: Express.Multer.File, fileName: string) {
+  //   const processedImage = await sharp(file.buffer)
+  //     .resize(1640, 856, {
+  //       fit: sharp.fit.cover,
+  //       position: sharp.strategy.entropy
+  //     })
+  //     .jpeg({
+  //       quality: 80
+  //     }).toBuffer();
+  //   return await this.s3.upload({
+  //     Bucket: this.s3Bucket,
+  //     Key: `project-covers/${fileName}`,
+  //     Body: processedImage,
+  //     ACL: "public-read",
+  //     ContentType: file.mimetype,
+  //     ContentDisposition: "inline"
+  //   }).promise();
+  // }
 
-  async uploadProjectCoverFile(file: Express.Multer.File, fileName: string) {
-    const processedImage = await sharp(file.buffer)
-      .resize(1640, 856, {
-        fit: sharp.fit.cover,
-        position: sharp.strategy.entropy
-      })
-      .jpeg({
-        quality: 80
-      }).toBuffer();
-    return await this.s3.upload({
-      Bucket: this.s3Bucket,
-      Key: `project-covers/${fileName}`,
-      Body: processedImage,
-      ACL: "public-read",
-      ContentType: file.mimetype,
-      ContentDisposition: "inline"
-    }).promise();
-  }
-
-  async uploadAvatarFile(file: Express.Multer.File, fileName: string) {
-    const processedImage = await sharp(file.buffer)
-      .resize(256, 256, {
-        fit: sharp.fit.cover,
-        position: sharp.strategy.entropy
-      }).jpeg({
-        quality: 80
-      })
-      .toBuffer();
-    return await this.s3.upload({
-      Bucket: this.s3Bucket,
-      Key: `avatars/${fileName}`,
-      Body: processedImage,
-      ACL: "public-read",
-      ContentType: file.mimetype,
-      ContentDisposition: "inline"
-    }).promise();
-  }
+  // async uploadAvatarFile(file: Express.Multer.File, fileName: string) {
+  //   const processedImage = await sharp(file.buffer)
+  //     .resize(256, 256, {
+  //       fit: sharp.fit.cover,
+  //       position: sharp.strategy.entropy
+  //     }).jpeg({
+  //       quality: 80
+  //     })
+  //     .toBuffer();
+  //   return await this.s3.upload({
+  //     Bucket: this.s3Bucket,
+  //     Key: `avatars/${fileName}`,
+  //     Body: processedImage,
+  //     ACL: "public-read",
+  //     ContentType: file.mimetype,
+  //     ContentDisposition: "inline"
+  //   }).promise();
+  // }
 
   async getSignedTaskAttachments(task: HydratedDocument<Task>) {
     let mappedAttachments: ProjectAttachmentWithDownloadUrl[] = [];
-    for (let attachment of (task.attachments || []) as HydratedDocument<ProjectAttachment>[]) {
-      const url = await this.getSignedUrl(attachment.storeKey);
-      const withDownloadUrl: ProjectAttachmentWithDownloadUrl = {
-        ...attachment.toJSON(),
-        url
-      };
-      mappedAttachments.push(withDownloadUrl);
-    }
+    // for (let attachment of (task.attachments || []) as HydratedDocument<ProjectAttachment>[]) {
+    //   const url = await this.getSignedUrl(attachment.storeKey);
+    //   const withDownloadUrl: ProjectAttachmentWithDownloadUrl = {
+    //     ...attachment.toJSON(),
+    //     url
+    //   };
+    //   mappedAttachments.push(withDownloadUrl);
+    // }
     return mappedAttachments;
   }
 }
