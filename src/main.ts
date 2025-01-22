afa462d18428f243b5f0b35497e26636d4a62f97
nmanhtuan17 <manhtuan17403@gmail.com>
2025-01-22 16:43:43 +0700

    :bug: update limit payload

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -34,6 +35,9 @@ async function bootstrap() {
       docExpansion: 'none',
     },
   });
+
+  app.use(json({ limit: "50mb" }));
+  app.use(urlencoded({ extended: true, limit: "50mb" }));
 
   await app.listen(process.env.PORT || 3000);
 }
