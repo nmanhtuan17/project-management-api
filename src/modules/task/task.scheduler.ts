f2ecb7cfa65fc93b21e12de76b23ec9e51e3ca60
Tuwn <manhtuan17403@gmail.com>
2025-04-29 19:19:52 +0700

    :BUG:

--- src/modules/task/task.scheduler.ts
+++ src/modules/task/task.scheduler.ts
@@ -8,7 +8,7 @@ export class TaskScheduler {
 
   constructor(private readonly taskService: TaskService) {}
 
-  @Cron(CronExpression.EVERY_MINUTE)
+  @Cron(CronExpression.EVERY_DAY_AT_9AM)
   async handleTaskDeadlines() {
     this.logger.log('Checking for upcoming task deadlines...');
     try {
